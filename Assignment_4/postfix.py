14bbccd88ebdcf0e8582f87253dd00e17e581141
Camille Church <36643562+Camille2985@users.noreply.github.com>
2021-12-20 11:21:51 -0500

    Commented solution and added functionality to read and write from files

--- Assignment_4/postfix.py
+++ Assignment_4/postfix.py
@@ -1,8 +1,28 @@
+# postfix.py
+# Author: Camille Church
+# Date 12/20/2021
+# Inputs: A file of equations in Postfix notation in lines format
+# Outputs: A file of solutions to the equation
+# Expected Behavior: Program will process file of equations and output the solutions to a
+#   to a single file
+
+
 class PostfixEquation:
     def __init__(self, equation):
         self.equation = equation
         self.solution = self.main()
 
+    # Solves the postfix equation by iterating by:
+    # 1. Breaking the equation into an array
+    # 2. Iterating through the array to find the operation elements
+    # 3. When it finds one:
+    #       a. Performs the operation on the previous 2 items in the array
+    #       b. Deletes three elements (2 operands and 1 operator)
+    #       c. Places the new value into the position of the first operand
+    #       d. Resets the iterator to 0
+    #       e. Continues loop
+    # 4. When loop is finished and there are no more operations to be preformed
+    #    the final value is returned
     def main(self):
         array_of_operands = self.equation.split()
         length = len(array_of_operands)
@@ -34,5 +54,20 @@ class PostfixEquation:
         return round(array_of_operands[0], 2)
 
 
-equation = PostfixEquation("4 2 8 / 5 2 + 2 * - +")
-print(equation.solution)
+# Takes in the input and output file locations, creates an instance of the PostfixEquation
+# class to solve the equation, and saves the solutions to the output file
+def main(input_file_loc, output_file_loc):
+    input_file = open(input_file_loc, 'r')
+    equations = input_file.readlines()
+    output_file = open(output_file_loc, 'w')
+
+    for equation in equations:
+        postfix_equation = PostfixEquation(equation)
+        output_file.write(str(postfix_equation.solution))
+        output_file.write("\n")
+
+    output_file.close()
+
+
+# Takes in a file of postfix equations and outputs the solutions
+main("input.txt", "output.txt")
